9ce29de7cae19d1581fbebe3695748f6ba5705c4
Tommi0x <tommaso.toloni@icloud.com>
2025-02-17 18:26:29 +0100

    Corretta formattazoine e aggiunti commenti

--- Prolog/huffman-codes.pl
+++ Prolog/huffman-codes.pl
@@ -1,24 +1,25 @@
-%% huffman-codes.pl
-%% Autori: [Inserisci qui i nomi dei membri del gruppo]
-%% Descrizione: Implementazione dei predicati per codifica/decodifica Huffman
-%%
-%% Rappresentazione:
-%%   - Una foglia viene rappresentata come:  leaf(Symbol, Weight).
-%%   - Un nodo interno viene rappresentato come:  tree(Left, Right, Weight).
-%%   - Le coppie simbolo-peso sono espresse come:  sw(Symbol, Weight).
-%%   - Le coppie simbolo-bits sono espresse come:  sb(Symbol, Bits).
+%%%% Toloni_Tommaso_914293
+%%%% Boschi_Leonardo_914255
 
-%%%%% COSTRUZIONE DELL'ALBERO DI HUFFMAN %%%%%
+%%%% huffman-codes.pl
+%%%% sw(Symbol, Weight) - rappresenta un simbolo con il suo peso
+%%%% sb(Symbol, Bits) - rappresenta un simbolo con la sua codifica in bit
+%%%% leaf(Symbol, Weight) - rappresenta una foglia dell'albero di Huffman
+%%%% tree(Left, Right, Weight) - rappresenta un nodo dell'albero di Huffman
 
-% Converte una coppia sw in una foglia.
+
+%%% GENERATE HUFFMAN TREE %%%
+
+% converte un termine sw(Symbol, Weight) in un termine leaf(Symbol, Weight)
 convert_sw_to_leaf(sw(Symbol, Weight), leaf(Symbol, Weight)).
 
-% Genera l'albero di Huffman da una lista di coppie sw/2.
+% hucodec_generate_huffman_tree(+SymbolsAndWeights, -HuffmanTree)
+% genera un albero huffman a partire da una lista di sw/2
 hucodec_generate_huffman_tree(SymbolsAndWeights, HuffmanTree) :-
     maplist(convert_sw_to_leaf, SymbolsAndWeights, Leaves),
     build_huffman_tree(Leaves, HuffmanTree).
 
-% Costruisce ricorsivamente l'albero combinando i due nodi con peso minore.
+%funzione di supporto per la generazione dell'albero di huffman
 build_huffman_tree([Tree], Tree).
 build_huffman_tree(Nodes, Tree) :-
     sort_nodes_by_weight(Nodes, Sorted),
@@ -30,30 +31,31 @@ build_huffman_tree(Nodes, Tree) :-
     append(Rest, [NewNode], NewNodes),
     build_huffman_tree(NewNodes, Tree).
 
-% Ordina la lista di nodi in base al peso, preservando duplicati.
+%ordinamento dei nodi in base al peso
 sort_nodes_by_weight(Nodes, SortedNodes) :-
-    % Associa ad ogni nodo il proprio peso
     map_list_to_pairs(weight, Nodes, Pairs),
-    % keysort è stabile e non elimina duplicati
     keysort(Pairs, SortedPairs),
     pairs_values(SortedPairs, SortedNodes).
 
+%estrazione del peso di un nodo/foglia
 weight(leaf(_, W), W).
 weight(tree(_, _, W), W).
 
-%%%%% GENERAZIONE DELLA TABELLA SIMBOLO -> BIT STRING %%%%%
 
-% Genera la tabella sb(Symbol, Bits) attraversando l'albero.
+%%% GENERATE SYMBOL BITS TABLE %%%
+
+% hucodec_generate_symbol_bits_table(+HuffmanTree, -Table)
+% genera una lista di coppie sb(Symbol, Bits) a partire da un albero huffman
 hucodec_generate_symbol_bits_table(HuffmanTree, Table) :-
     traverse_tree(HuffmanTree, [], Table).
 
+%attraversamento dell'albero e generazione della tabella
 traverse_tree(leaf(Symbol, _), Acc, [sb(Symbol, Bits)]) :-
     Acc = [],
     Bits = [0].
 traverse_tree(leaf(Symbol, _), Acc, [sb(Symbol, Bits)]) :-
     Acc \= [],
     Bits = Acc.
-
 traverse_tree(tree(Left, Right, _), Acc, Table) :-
     append(Acc, [0], AccLeft),
     traverse_tree(Left, AccLeft, TableLeft),
@@ -61,38 +63,42 @@ traverse_tree(tree(Left, Right, _), Acc, Table) :-
     traverse_tree(Right, AccRight, TableRight),
     append(TableLeft, TableRight, Table).
 
-%%%%% CODIFICA %%%%%
+
+%%% ENCODE %%%
 
 % hucodec_encode(+Message, +HuffmanTree, -Bits)
-% DATA: Message è una lista di simboli (ad es. [a,b,c,...])
+% codifica un messaggio a partire da un albero huffman
 hucodec_encode(Message, HuffmanTree, Bits) :-
     hucodec_generate_symbol_bits_table(HuffmanTree, Table),
     encode_message(Message, Table, Bits).
 
+%codifica del messaggio
 encode_message([], _Table, []).
 encode_message([Symbol|Rest], Table, Bits) :-
     member(sb(Symbol, Code), Table),
     encode_message(Rest, Table, BitsRest),
     append(Code, BitsRest, Bits).
 
-%%%%% DECODIFICA %%%%%
+
+%%% DECODE %%%
 
 % hucodec_decode(+Bits, +HuffmanTree, -Message)
+% decodifica una sequenza di bit a partire da un albero huffman
 hucodec_decode(Bits, HuffmanTree, Message) :-
     HuffmanTree \= leaf(_, _),
     decode_bits(Bits, HuffmanTree, HuffmanTree, Message).
-
 hucodec_decode(Bits, leaf(Symbol, _), Message) :-
-    length(Bits, N),                % Determine the number of bits
-    length(Message, N),             % Ensure Message has the same length
-    maplist(=(Symbol), Message).     % Assign the Symbol to each position in Message
+    length(Bits, N),                
+    length(Message, N),             
+    maplist(=(Symbol), Message).     
 
+%decodifica dei bit
 decode_bits([], _HuffmanTree, _Current, []).
 decode_bits(Bits, HuffmanTree, Current, [Symbol|RestMessage]) :-
     traverse_for_symbol(Bits, Current, Symbol, RemainingBits),
     decode_bits(RemainingBits, HuffmanTree, HuffmanTree, RestMessage).
 
-% traverse_for_symbol(+Bits, +CurrentNode, -Symbol, -RemainingBits)
+%attraversamento dell'albero per trovare il simbolo corrispondente
 traverse_for_symbol(Bits, leaf(Symbol, _), Symbol, Bits) :- !.
 traverse_for_symbol([Bit|RestBits], tree(Left, _, _), Symbol, RemainingBits) :-
     Bit =:= 0,
@@ -101,40 +107,45 @@ traverse_for_symbol([Bit|RestBits], tree(_, Right, _), Symbol, RemainingBits) :-
     Bit =:= 1,
     traverse_for_symbol(RestBits, Right, Symbol, RemainingBits).
 
-%%%%% CODIFICA DI UN FILE %%%%%
+
+%%% ENCODE FILE %%%
 
 % hucodec_encode_file(+Filename, +HuffmanTree, -Bits)
+% codifica un file a partire da un albero huffman
 hucodec_encode_file(Filename, HuffmanTree, Bits) :-
     read_file_to_string(Filename, String),
     string_chars(String, Message),
     hucodec_encode(Message, HuffmanTree, Bits).
 
-% Legge il contenuto di un file in una stringa.
+%lettura del file
 read_file_to_string(Filename, String) :-
     open(Filename, read, Stream),
     read_stream_to_codes(Stream, Codes),
     close(Stream),
     string_codes(String, Codes).
 
+%lettura dello stream
 read_stream_to_codes(Stream, Codes) :-
     read_stream_to_codes(Stream, [], Codes).
-
 read_stream_to_codes(Stream, Acc, Codes) :-
     get_code(Stream, Code),
     process_code(Code, Stream, Acc, Codes).
 
+%formattazione dei codici
 process_code(-1, _, Acc, Codes) :-
     reverse(Acc, Codes).
 process_code(Code, Stream, Acc, Codes) :-
     Code \= -1,
     read_stream_to_codes(Stream, [Code|Acc], Codes).
 
-%%%%% STAMPA DELL'ALBERO %%%%%
+%%% PRINT HUFFMAN TREE %%%
 
 % hucodec_print_huffman_tree(+HuffmanTree)
+% stampa a video l'albero huffman
 hucodec_print_huffman_tree(Tree) :-
     print_tree(Tree, 0).
 
+%formattazione per la stampa dell'albero
 print_tree(leaf(Symbol, Weight), Indent) :-
     format('~*cLeaf: ~w (Weight: ~w)~n', [Indent, 32, Symbol, Weight]).
 print_tree(tree(Left, Right, Weight), Indent) :-
